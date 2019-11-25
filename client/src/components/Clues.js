5b663ec0198f5c5db575464ca76d76c4afb506bb
Jax Clark <jamaecla@gmail.com>
2019-11-25 15:18:53 -0700

    jax update adding clues and clueslist components

---
+++ client/src/components/Clues.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {withClue} from '../context/ClueProvider'
+
+const Clues = (props) => {
+    return(
+        <div>
+            blah blah fuck this noise
+            {this.props.clue}
+        </div>
+    )
+}
+
+export default withClue(Clues)
