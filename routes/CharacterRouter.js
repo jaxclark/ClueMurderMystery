d6b1b879a847834ebd2103b9ae87f9ba47e587dd
Laura Daybell <laura.l.daybell@gmail.com>
2019-11-25 08:58:59 -0700

    routes completed and functioning

--- routes/CharacterRouter.js
+++ routes/CharacterRouter.js
@@ -3,13 +3,55 @@ const characterRouter = express.Router()
 const Character = require('../models/Character')
 
 
-characterRouter.get("/", (req, res) => {
-    Character.find((err, characters) => {
-        if (err) {
-            return res.status(500).send(err)
-        }
-        return res.status(200).send(characters)
+characterRouter.route('/')
+    .get ((req, res) => {
+        Character.find((err, characters) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(characters)
+        })
     })
-})
+    .post((req, res) => {
+        const newCharacter = new Character(req.body)
+        newCharacter.save(err => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(newCharacter)
+        })
+    })
+
+characterRouter.route('/:_id')
+    .get((req, res) => {
+        Character.findById(req.params._id, (err, characters) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(characters)
+        })
+    })
+
+    .put((req, res) => {
+        Character.findByIdAndUpdate(req.params._id, req.body, {new:true}, (err, character) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(character)
+        })
+    })
+
+    .delete((req, res) => {
+        Character.findByIdAndRemove(req.params._id, (err, character) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            const response = {
+                message: "Character successfully deleted",
+                id: character._id
+            }
+            return res.status(200).send(response)
+        })
+    })
 
 module.exports = characterRouter
--- routes/WeaponRouter.js
+++ routes/WeaponRouter.js
@@ -3,14 +3,56 @@ const weaponRouter = express.Router()
 const Weapon = require('../models/weapon')
 
 
-weaponRouter.get ("/", (req, res) => {
-    Weapon.find((err, weapons) => {
-        if (err) {
-            return res.status(500).send(err)
-        }
-        return res.status(200).send(weapons)
+weaponRouter.route('/')
+    .get ((req, res) => {
+        Weapon.find((err, weapons) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(weapons)
+        })
     })
-})
+    .post((req, res) => {
+        const newWeapon = new Weapon(req.body)
+        newWeapon.save(err => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(newWeapon)
+        })
+    })
+
+weaponRouter.route('/:_id')
+    .get((req, res) => {
+        Weapon.findById(req.params._id, (err, weapons) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(weapons)
+        })
+    })
+
+    .put((req, res) => {
+        Weapon.findByIdAndUpdate(req.params._id, req.body, {new:true}, (err,weapon) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            return res.status(200).send(weapon)
+        })
+    })
+
+    .delete((req, res) => {
+        Weapon.findByIdAndRemove(req.params._id, (err, weapon) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            const response = {
+                message: "Weapon successfully deleted",
+                id: weapon._id
+            }
+            return res.status(200).send(response)
+        })
+    })
 
 
 
