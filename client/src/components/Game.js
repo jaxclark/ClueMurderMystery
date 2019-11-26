a24c6d9c16877f475a94b0343a727de78052cb50
Betsy Popek <betsypopek@gmail.com>
2019-11-26 14:59:52 -0700

    Game Page image and some styling

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -21,16 +21,19 @@ class Game extends Component {
 
     render() {
         return(
-            <div>
-                <form>
-                    <button onClick={this.handleClick} name='Miss Scarlet' value='Miss Scarlet' >Miss Scarlet</button>
-                    <button onClick={this.handleClick} name='Colonel Mustard' value='Colonel Mustard' >Colonel Mustard</button>
-                    <button onClick={this.handleClick} name='Mrs. White' value='Mrs. White' >Mrs. White</button>
-                    <button onClick={this.handleClick} name='Mr. Green' value='Mr. Green' >Mr. Green</button>
-                    <button onClick={this.handleClick} name='Mrs. Peacock' value='Mrs. Peacock' >Mrs. Peacock</button>
-                    <button onClick={this.handleClick} name='Professor Plum' value='Professor Plum' >Professor Plum</button>
+            <div className='gameDiv'>
+                <form className='gameForm'>
+                    <button onClick={this.handleClick} name='Miss Scarlet' value='Miss Scarlet' className='gameInterviewButton'>Miss Scarlet</button>
+                    <button onClick={this.handleClick} name='Colonel Mustard' value='Colonel Mustard' className='gameInterviewButton' >Colonel Mustard</button>
+                    <button onClick={this.handleClick} name='Mrs. White' value='Mrs. White' className='gameInterviewButton'>Mrs. White</button>
+                    <button onClick={this.handleClick} name='Mr. Green' value='Mr. Green' className='gameInterviewButton'>Mr. Green</button>
+                    <button onClick={this.handleClick} name='Mrs. Peacock' value='Mrs. Peacock' className='gameInterviewButton'>Mrs. Peacock</button>
+                    <button onClick={this.handleClick} name='Professor Plum' value='Professor Plum' className='gameInterviewButton'>Professor Plum</button>
                 </form>
-                <div>{`Interview Suspect: ${this.state.suspectName}`}</div>
+                <div className='gameClueDiv'>
+                   clue container test
+                </div>
+                <div className='gameInterviewSuspect'>{`Interview Suspect: ${this.state.suspectName}`}</div>
                 <CluesListContainer />
             </div>
         )
